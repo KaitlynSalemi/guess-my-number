dbded29a0d56b9a49046525a51c9275588f466ac
Kaitlyn Salemi <kaitlyn.salemi@gmail.com>
2020-11-13 14:52:05 -0600

    number comparison

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 'use strict';
+
+const message = document.querySelector('.message');
+const secretNumber = Math.trunc(Math.random() * 10 + 1);
+// console.log(secretNumber);
+document.querySelector('.number').textContent = secretNumber;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  //   console.log(guess, typeof guess);
+  if (!guess) {
+    message.textContent = 'Please enter a number!';
+  } else if (guess === secretNumber) {
+    message.textContent = 'Correct number!';
+  } else if (guess > secretNumber) {
+    message.textContent = 'Too high!';
+  } else if (guess < secretNumber) {
+    message.textContent = 'Too low!';
+  }
+});
