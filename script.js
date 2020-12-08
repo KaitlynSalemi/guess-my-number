eb605b7f92928fd7f3993134e3ffd3bfa1b15331
Kaitlyn Salemi <kaitlyn.salemi@gmail.com>
2020-12-08 13:50:02 -0600

    manipulating style when answer is correct

--- script.js
+++ script.js
@@ -9,11 +9,18 @@ document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
-  //   console.log(guess, typeof guess);
+
+  // When there is no input
   if (!guess) {
     message.textContent = 'Please enter a number!';
+
+    // When the guess is correct
   } else if (guess === secretNumber) {
     message.textContent = 'Correct number!';
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+
+    // When the guess is too high
   } else if (guess > secretNumber) {
     if (score > 1) {
       message.textContent = 'Too high!';
@@ -23,6 +30,8 @@ document.querySelector('.check').addEventListener('click', function () {
       message.textContent = 'You loose the game!';
       score.textContent = 0;
     }
+
+    // When the guess is too low
   } else if (guess < secretNumber) {
     if (score > 1) {
       message.textContent = 'Too low!';
